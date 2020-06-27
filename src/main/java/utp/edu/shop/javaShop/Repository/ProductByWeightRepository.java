75ae7bec2fbe906e30bfbb2123862e4a25c4f286
Pawell641 <plewandowski555@gmail.com>
2020-06-28 00:08:02 +0200

    Add possibility to return product

--- src/main/java/utp/edu/shop/javaShop/Repository/ProductByWeightRepository.java
+++ src/main/java/utp/edu/shop/javaShop/Repository/ProductByWeightRepository.java
@@ -3,5 +3,5 @@ package utp.edu.shop.javaShop.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import utp.edu.shop.javaShop.Models.ProductByWeight;
 
-public interface ProductByWeightRepository extends JpaRepository<ProductByWeight, Integer> { }
+public interface ProductByWeightRepository extends JpaRepository<ProductByWeight, Integer> {}
 
--- src/main/resources/static/scripts/returnForm.js
+++ src/main/resources/static/scripts/returnForm.js
@@ -1,13 +1,13 @@
-function order(productId){
+function acceptForm(){
     var name = document.getElementById('name').value.toString();
     var surname = document.getElementById('surname').value.toString();
-    var id = productId;
+    var orderId = document.getElementById('orderNumber').value.toString();
 
     $.ajax({
-        url : "/save/order/" + id + "/" + name + "/" + surname,
+        url : "/cancel/order/" + orderId + "/" + name + "/" + surname,
         method : "post",
         data : JSON.stringify({
-            "productId": id,
+            "productId": orderId,
             "name": name,
             "surname": surname
         }),
